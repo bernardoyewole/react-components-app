eec88217593ea73d4af9033129bf2c440dad3657
Bernard <oyebenard@gmail.com>
2024-06-03 10:13:35 -0500

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header>
             <nav class="container">
                 <ul>
-                    <li><a className='nav-title' href="">React</a></li>
+                    <li><a className='nav-title' href="/#">React</a></li>
                     <li><a href="https://react.dev/learn/describing-the-ui">The UI</a></li>
                     <li><a href="https://react.dev/learn/adding-interactivity">Interactivity</a></li>
                     <li><a href="https://react.dev/learn/managing-state">Managing State</a></li>
